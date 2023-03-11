bb4f7446b1f18232e5724eeb91823f5286c39722
wahyufahrozi <wahyufahrozi97@gmail.com>
2023-03-12 01:57:34 +0700

    final

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { AddressLink } from "../components/AddressLink";
+import PlaceGallery from "../components/PlaceGallery";
+import { BookingDates } from "../components/BookingDates";
 
 const Booking = () => {
   const { id } = useParams();
@@ -22,18 +25,18 @@ const Booking = () => {
   return (
     <div className="my-8">
       <h1 className="text-3xl">{booking.place.title}</h1>
-      {/* <AddressLink className="my-2 block">{booking.place.address}</AddressLink> */}
+      <AddressLink className="my-2 block">{booking.place.address}</AddressLink>
       <div className="bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between">
         <div>
           <h2 className="text-2xl mb-4">Your booking information:</h2>
-          {/* <BookingDates booking={booking} /> */}
+          <BookingDates booking={booking} />
         </div>
         <div className="bg-primary p-6 text-white rounded-2xl">
           <div>Total price</div>
           <div className="text-3xl">${booking.price}</div>
         </div>
       </div>
-      {/* <PlaceGallery place={booking.place} /> */}
+      <PlaceGallery place={booking.place} />
     </div>
   );
 };
